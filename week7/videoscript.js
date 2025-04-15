3a9822014a57a57fdfc77af95647bf7c98277ccf
Eva Evgenidis <evanthia-e@hotmail.com>
2025-04-15 12:17:07 +1000

    +week7 complete

--- week7/videoscript.js
+++ week7/videoscript.js
@@ -22,3 +22,23 @@ pauseButton.addEventListener("click", pauseVideo);
 function pauseVideo() {
   myVideo.pause();
 }
+
+// play pause
+
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+playPauseButton.addEventListener("click", toggleVideo);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+
+function toggleVideo() {
+  if (myVideo.paused || myVideo.ended) {
+    myVideo.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+  }
+}
