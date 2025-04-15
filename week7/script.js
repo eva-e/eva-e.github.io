901d73bb39d2937de0d38ec1f13a9e5e0d592aae
Eva Evgenidis <evanthia-e@hotmail.com>
2025-04-15 10:53:01 +1000

    +week 7 audio

---
+++ week7/script.js
@@ -0,0 +1,47 @@
+const topHeading = document.querySelector("#top-heading");
+console.log(topHeading);
+
+let myNewHeading = "audio is currently playing";
+
+const airportAudio = document.querySelector("#airport-audio");
+console.log(airportAudio);
+
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+
+playButton.addEventListener("click", playAudio);
+
+function playAudio() {
+  myNewHeading = "Audio is currently playing";
+  airportAudio.play();
+  topHeading.textContent = myNewHeading;
+}
+
+// pausing audio
+
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+playButton.addEventListener("click", pauseAudio);
+
+function pauseAudio() {
+  myNewHeading = "Audio is currently paused";
+  airportAudio.play();
+  topHeading.textContent = myNewHeading;
+}
+
+// pop sound
+
+const popSound = document.querySelector("#pop-audio");
+console.log(popSound);
+
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+popButton.addEventListener("click", popAudio);
+
+function popAudio() {
+  myNewHeading = "Popping sound";
+  popSound.play();
+  topHeading.textContent = myNewHeading;
+}
