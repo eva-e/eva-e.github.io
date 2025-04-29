b71f5ee1bde9b8755add17a85304270d293d46af
Eva Evgenidis <evanthia-e@hotmail.com>
2025-04-30 02:10:21 +1000

    +update assignment 2 - testing video

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,73 +1,88 @@
-// AUDIO PLAYER ONLY
+// VIDEO BACKGROUND
+// Fetch the video
+const video1 = document.querySelector("#my-video1");
+const playPauseButton1 = document.querySelector("#play-pause-button1");
 
-const topHeading = document.querySelector("#top-heading");
-console.log(topHeading);
-
-let myNewHeading = "audio is currently playing";
-
-const dangoAudio = document.querySelector("#dango-audio");
-console.log(dangoAudio);
-
-// playing sound
-const playButton = document.querySelector("#play-button");
-console.log(playButton);
-
-playButton.addEventListener("click", playAudio);
-
-function playAudio() {
-  myNewHeading = "Audio is currently playing";
-  dangoAudio.play();
-  topHeading.textContent = myNewHeading;
+function myFunction() {
+  if (video1.paused) {
+    video1.play();
+    playPauseButton1.innerHTML = "Pause";
+  } else {
+    video1.pause();
+    playPauseButton1.innerHTML = "Play";
+  }
 }
+
+// // AUDIO PLAYER ONLY
+
+// const topHeading = document.querySelector("#top-heading");
+// console.log(topHeading);
+
+// let myNewHeading = "audio is currently playing";
+
+// const dangoAudio = document.querySelector("#dango-audio");
+// console.log(dangoAudio);
+
+// // playing sound
+// const playButton = document.querySelector("#play-button");
+// console.log(playButton);
+
+// playButton.addEventListener("click", playAudio);
+
+// function playAudio() {
+//   myNewHeading = "Audio is currently playing";
+//   dangoAudio.play();
+//   topHeading.textContent = myNewHeading;
+// }
 
 // PLAYLIST LOGIC - USEFUL FOR ASSIGNMENT 2-
 // website feature change between lightmode and darkmode, prompt the user to use darkmode lightmode or warm mode
-const playlist = [
-  { id: 1, src: "stardust.mp4" },
-  { id: 2, src: "zenscape.mp4" },
-  {
-    id: 3,
-    src: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
-    name: "Music Video",
-  },
-];
+// const playlist = [
+//   { id: 1, src: "stardust.mp4" },
+//   { id: 2, src: "zenscape.mp4" },
+//   {
+//     id: 3,
+//     src: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
+//     name: "Music Video",
+//   },
+// ];
 
-// stardust
-// playlist logic
-const stardustButton = document.querySelector("#stardust-vid-button");
-console.log(stardustButton);
+// // stardust
+// // playlist logic
+// const stardustButton = document.querySelector("#stardust-vid-button");
+// console.log(stardustButton);
 
-// listen to click button
-stardustButton.addEventListener("click", function chooseStardust() {
-  chooseVideo(0);
-});
+// // listen to click button
+// stardustButton.addEventListener("click", function chooseStardust() {
+//   chooseVideo(0);
+// });
 
-// zenscape
-// playlist logic
-const zenscapeButton = document.querySelector("#zenscape-vid-button");
-console.log(zenscapeButton);
+// // zenscape
+// // playlist logic
+// const zenscapeButton = document.querySelector("#zenscape-vid-button");
+// console.log(zenscapeButton);
 
-// listen to click button
-zenscapeButton.addEventListener("click", function chooseZenscape() {
-  chooseVideo(1);
-});
+// // listen to click button
+// zenscapeButton.addEventListener("click", function chooseZenscape() {
+//   chooseVideo(1);
+// });
 
-// music video
-// playlist logic
-const musicVidButton = document.querySelector("#musicvideo-vid-button");
-console.log(musicVidButton);
+// // music video
+// // playlist logic
+// const musicVidButton = document.querySelector("#musicvideo-vid-button");
+// console.log(musicVidButton);
 
-// listen to click button
-musicVidButton.addEventListener("click", function chooseMusicVideo() {
-  chooseVideo(2);
-});
+// // listen to click button
+// musicVidButton.addEventListener("click", function chooseMusicVideo() {
+//   chooseVideo(2);
+// });
 
-function chooseVideo(no) {
-  myVideo.src = playlist[no].src;
-  console.log(myVideo.src);
-  myVideo.load();
-  myVideo.play();
-}
+// function chooseVideo(no) {
+//   myVideo.src = playlist[no].src;
+//   console.log(myVideo.src);
+//   myVideo.load();
+//   myVideo.play();
+// }
 
 // const video = document.querySelector("#custom-video-player");
 // const playPauseBtn = document.querySelector("#play-pause-btn");
