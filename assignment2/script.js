d1bb6dcaa51bd22fe3322bce8d3c5e56169eaca0
Eva Evgenidis <evanthia-e@hotmail.com>
2025-04-30 22:10:21 +1000

    +assignment 2 update - add fonts + media player

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,20 +1,43 @@
-// VIDEO BACKGROUND
-// Fetch the video
-const video1 = document.querySelector("#my-video1");
-const playPauseButton1 = document.querySelector("#play-pause-button1");
-
-function myFunction() {
-  if (video1.paused) {
-    video1.play();
-    playPauseButton1.innerHTML = "Pause";
+// VIDEO PLAYER
+const video = document.querySelector("#my-video1");
+const playPauseBtn = document.querySelector("#play-pause-button1");
+const playPauseImg = document.querySelector("#play-pause-img");
+const progressBar = document.querySelector("#progress-bar-fill");
+video.removeAttribute("controls");
+// playPauseBtn.addEventListener("click", togglePlayPause);
+video.addEventListener("timeupdate", updateProgressBar);
+function togglePlayPause() {
+  if (video.paused || video.ended) {
+    video.play();
+    playPauseImg.src =
+      "https://img.icons8.com/ios-glyphs/30/fdafc9/pause--v1.png";
   } else {
-    video1.pause();
-    playPauseButton1.innerHTML = "Play";
+    video.pause();
+    playPauseImg.src =
+      "https://img.icons8.com/fluency-systems-filled/48/fdafc9/play.png";
   }
 }
+function updateProgressBar() {
+  const value = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = value + "%";
+}
 
-// // AUDIO PLAYER ONLY
+// VIDEO BACKGROUND FROM W3SCHOOLS
+// Fetch the video
+// const video1 = document.querySelector("#my-video1");
+// const playPauseButton1 = document.querySelector("#play-pause-button1");
 
+// function myFunction() {
+//   if (video1.paused) {
+//     video1.play();
+//     playPauseButton1.innerHTML = "Pause";
+//   } else {
+//     video1.pause();
+//     playPauseButton1.innerHTML = "Play";
+//   }
+// }
+
+// // AUDIO PLAYER
 // const topHeading = document.querySelector("#top-heading");
 // console.log(topHeading);
 
@@ -35,73 +58,50 @@ function myFunction() {
 //   topHeading.textContent = myNewHeading;
 // }
 
-// PLAYLIST LOGIC - USEFUL FOR ASSIGNMENT 2-
-// website feature change between lightmode and darkmode, prompt the user to use darkmode lightmode or warm mode
-// const playlist = [
-//   { id: 1, src: "stardust.mp4" },
-//   { id: 2, src: "zenscape.mp4" },
-//   {
-//     id: 3,
-//     src: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
-//     name: "Music Video",
-//   },
-// ];
+// PLAYLIST LOGIC - USEFUL FOR ASSIGNMENT 2
+const playlist = [
+  { id: 1, src: "stardust.mp4" },
+  { id: 2, src: "zenscape.mp4" },
+  {
+    id: 3,
+    src: "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
+    name: "Music Video",
+  },
+];
 
-// // stardust
-// // playlist logic
-// const stardustButton = document.querySelector("#stardust-vid-button");
-// console.log(stardustButton);
+// stardust
+// playlist logic
+const stardustButton = document.querySelector("#stardust-vid-button");
+console.log(stardustButton);
 
-// // listen to click button
-// stardustButton.addEventListener("click", function chooseStardust() {
-//   chooseVideo(0);
-// });
+// listen to click button
+stardustButton.addEventListener("click", function chooseStardust() {
+  chooseVideo(0);
+});
 
-// // zenscape
-// // playlist logic
-// const zenscapeButton = document.querySelector("#zenscape-vid-button");
-// console.log(zenscapeButton);
+// zenscape
+// playlist logic
+const zenscapeButton = document.querySelector("#zenscape-vid-button");
+console.log(zenscapeButton);
 
-// // listen to click button
-// zenscapeButton.addEventListener("click", function chooseZenscape() {
-//   chooseVideo(1);
-// });
+// listen to click button
+zenscapeButton.addEventListener("click", function chooseZenscape() {
+  chooseVideo(1);
+});
 
-// // music video
-// // playlist logic
-// const musicVidButton = document.querySelector("#musicvideo-vid-button");
-// console.log(musicVidButton);
+// music video
+// playlist logic
+const musicVidButton = document.querySelector("#musicvideo-vid-button");
+console.log(musicVidButton);
 
-// // listen to click button
-// musicVidButton.addEventListener("click", function chooseMusicVideo() {
-//   chooseVideo(2);
-// });
+// listen to click button
+musicVidButton.addEventListener("click", function chooseMusicVideo() {
+  chooseVideo(2);
+});
 
-// function chooseVideo(no) {
-//   myVideo.src = playlist[no].src;
-//   console.log(myVideo.src);
-//   myVideo.load();
-//   myVideo.play();
-// }
-
-// const video = document.querySelector("#custom-video-player");
-// const playPauseBtn = document.querySelector("#play-pause-btn");
-// const playPauseImg = document.querySelector("#play-pause-img");
-// const progressBar = document.querySelector("#progress-bar-fill");
-// video.removeAttribute("controls");
-// // playPauseBtn.addEventListener("click", togglePlayPause);
-// video.addEventListener("timeupdate", updateProgressBar);
-// function togglePlayPause() {
-//   if (video.paused || video.ended) {
-//     video.play();
-//     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
-//   } else {
-//     video.pause();
-//     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
-//   }
-// }
-// function updateProgressBar() {
-//   const value = (video.currentTime / video.duration) * 100;
-//   progressBar.style.width = value + "%";
-// }
-// Add other functionalities here
+function chooseVideo(no) {
+  myVideo.src = playlist[no].src;
+  console.log(myVideo.src);
+  myVideo.load();
+  myVideo.play();
+}
