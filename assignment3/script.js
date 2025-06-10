03769560d4f60df6c8a8b3ac039bbabc9df6090e
Eva Evgenidis <evanthia-e@hotmail.com>
2025-06-10 18:31:02 +1000

    +update html + js - start plant buttons

--- assignment3/script.js
+++ assignment3/script.js
@@ -2,31 +2,86 @@
 // The drag action will be for dragging the plant to a designated area on the shelving.
 // The click action is for choosing a plant and watering the plant.
 // Three buttons for the plant options and one button for the watering can.
-const card = document.querySelector(".card");
-console.log(card);
 
-let draggedCard = null;
+// Change image from no plant to Grown plant
+const myImg = document.getElementById("myImg");
+const myButton = document.getElementById("myButton");
 
-card.addEventListener("dragstart", function () {
-  draggedCard = card;
+myButton.addEventListener("click", function () {
+  myImg.src = "plant-grown1.png";
 });
 
-const dropbox = document.querySelector(".dropbox");
-console.log(dropbox);
+const myImg2 = document.getElementById("myImg2");
+const myButton2 = document.getElementById("myButton2");
 
-dropbox.addEventListener("dragover", function (e) {
-  e.preventDefault();
+myButton.addEventListener("click", function () {
+  myImg2.src = "plant-grown2.png";
 });
 
-dropbox.addEventListener("drop", function () {
-  if (draggedCard) {
-    const clone = draggedCard;
-    dropbox.appendChild(clone);
-    clone.addEventListener("click", function () {
-      clone.classList.toggle("flip");
-    });
-    draggedCard = null;
-  }
+const myImg3 = document.getElementById("myImg2");
+const myButton3 = document.getElementById("myButton2");
+
+myButton.addEventListener("click", function () {
+  myImg2.src = "plant-grown3.png";
 });
+
+// const card = document.querySelector(".card");
+// console.log(card);
+
+// let draggedCard = null;
+
+// card.addEventListener("dragstart", function () {
+//   draggedCard = card;
+// });
+
+// const dropbox = document.querySelector(".dropbox");
+// console.log(dropbox);
+
+// dropbox.addEventListener("dragover", function (e) {
+//   e.preventDefault();
+// });
+
+// dropbox.addEventListener("drop", function () {
+//   if (draggedCard) {
+//     const clone = draggedCard;
+//     dropbox.appendChild(clone);
+//     clone.addEventListener("click", function () {
+//       clone.classList.toggle("flip");
+//     });
+//     draggedCard = null;
+//   }
+// });
 
 //   dropbox.innerHTML = draggedCard;
+
+// DRAG + DROP
+// let draggedElement = null;
+
+// const colorBox = document.querySelector(".colorbox");
+// const dropBox = document.querySelector(".dropbox");
+
+// colorBox.addEventListener("dragstart", startDrag);
+
+// function startDrag() {
+//   draggedElement = colorBox;
+// }
+
+// dropBox.addEventListener("dragover", endDrag);
+
+// function endDrag(event) {
+//   event.preventDefault();
+// }
+
+// dropBox.addEventListener("drop", handleDrop);
+
+// function handleDrop() {
+//   if (draggedElement) {
+//     const color = window
+//       .getComputedStyle(draggedElement)
+//       .getPropertyValue("background-color");
+//     // const color = draggedElement.style.backgroundColor;
+//     dropBox.style.backgroundColor = color;
+//     dropBox.textContent = "Dropped!";
+//     draggedElement = null;
+//   }
+// }
