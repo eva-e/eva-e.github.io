103e799bc66ef4b51966662cc19d18ccce0ba942
Eva Evgenidis <evanthia-e@hotmail.com>
2025-06-13 00:00:22 +1000

    +clean js - drawing pixel art plants

--- assignment3/script.js
+++ assignment3/script.js
@@ -20,58 +20,27 @@ function togglePlayPause() {
 }
 
 // Change image from no plant to Grown plant
+// images will be named plant-grown1 and so on
 const myImg = document.getElementById("myImg");
 const myButton = document.getElementById("myButton");
 
 myButton.addEventListener("click", function () {
-  myImg.src = "images/plant-grown1.png";
+  myImg.src = "images/plant3.png";
 });
 
 const myImg2 = document.getElementById("myImg2");
 const myButton2 = document.getElementById("myButton2");
 
 myButton.addEventListener("click", function () {
-  myImg2.src = "images/plant-grown2.png";
+  myImg2.src = "images/plant3.png";
 });
 
 const myImg3 = document.getElementById("myImg2");
 const myButton3 = document.getElementById("myButton2");
 
 myButton.addEventListener("click", function () {
-  myImg2.src = "images/plant-grown3.png";
+  myImg2.src = "images/plant2.png";
 });
-
-// DRAG + DROP
-// let draggedElement = null;
-
-// const colorBox = document.querySelector(".colorbox");
-// const dropBox = document.querySelector(".dropbox");
-
-// colorBox.addEventListener("dragstart", startDrag);
-
-// function startDrag() {
-//   draggedElement = colorBox;
-// }
-
-// dropBox.addEventListener("dragover", endDrag);
-
-// function endDrag(event) {
-//   event.preventDefault();
-// }
-
-// dropBox.addEventListener("drop", handleDrop);
-
-// function handleDrop() {
-//   if (draggedElement) {
-//     const color = window
-//       .getComputedStyle(draggedElement)
-//       .getPropertyValue("background-color");
-//     // const color = draggedElement.style.backgroundColor;
-//     dropBox.style.backgroundColor = color;
-//     dropBox.textContent = "Dropped!";
-//     draggedElement = null;
-//   }
-// }
 
 // DRAG DROP CARD
 // const card = document.querySelector(".card");
